a1fd3f640439b11aecac125f8c5156809f63b423
Aslan1st <aslan1st@mac.com>
2017-10-01 05:52:46 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -66,7 +66,7 @@ describe('loops', () => {
   })
 })
 function forLoop(array){
-    for (var i = 0; i < 25; i++){ 
+    for (var i = 0; i < 25; i++){
     if i = 1 console.log("I am ${i} strange loop.")
     else console.log("I am ${i} strange loops.")
 }}
